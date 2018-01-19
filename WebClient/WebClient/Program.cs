e95f6252ab397d80cba0876caad7c708ad340c25
Hosfix-DELL <hosfix@gmail.com>
2018-01-19 15:07:30 +0100

    Cliente

---
+++ WebClient/WebClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            WebClient.ServiceReference1.IService webService = new WebClient.ServiceReference1.ServiceClient();
+            Tuple<string, decimal> tuple = webService.CalculateTip("sku", "AUD");
+            Console.WriteLine(webService.GetJsonOrders());
+            Console.ReadLine();
+        }
+    }
+}
